4edd2a4a94900fa08e80e9d732d30ed4c4740c21
falahh6 <falahsss900@gmail.com>
2024-12-11 00:06:56 +0530

    ui fixes

---
+++ src/app/mailbox/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div className="p-4">
+      <h1>Mailbox</h1>
+    </div>
+  );
+};
+
+export default Page;
--- src/components/mailbox/selector.tsx
+++ src/components/mailbox/selector.tsx
@@ -18,21 +18,21 @@ export const Selector = () => {
       {isLoading ? (
         <div className="h-12 w-52 border bg-gray-100 animate-pulse rounded-xl"></div>
       ) : (
-        <Select value={integrations[0].id}>
+        <Select value={integrations[0]?.id}>
           <SelectTrigger className="text-left w-fit h-fit rounded-xl ring-0">
             <SelectValue placeholder="one" className="text-left" />
           </SelectTrigger>
           <SelectContent className="rounded-xl">
             {integrations.map((integration, idx) => (
-              <SelectItem key={idx} value={integration.id}>
+              <SelectItem
+                key={idx}
+                value={integration.id}
+                className="rounded-lg"
+              >
                 <div className="flex flex-row gap-2 w-full">
                   <div className="bg-neutral-50 h-6 w-6 rounded-full p-1">
-                    {integration.provider === "Google" && (
-                      <Icons.google className="" />
-                    )}
-                    {integration.provider === "Outlook" && (
-                      <Icons.outlook className="h-4 w-4" />
-                    )}
+                    {integration.provider === "Google" && <Icons.google />}
+                    {integration.provider === "Outlook" && <Icons.outlook />}
                   </div>
                   <div className="text-xs">
                     <h4 className="font-bold">{integration.provider}</h4>
